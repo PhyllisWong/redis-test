9ec9d9afed6b14206284a74daa0d8e9c013ca9c3
Phyllis Wong <phyllis@Phylliss-MacBook-Pro.local>
2018-05-22 17:41:43 -0700

    Add postgres test database for development

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
+import json
 from flask import Flask, request
+from models import db
+
 
 app = Flask(__name__)
+# app config
+db.init_app(app)
+
+
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'password',
+    'db': 'my_database',
+    'host': 'localhost',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/person')
+def person_route():
+    person = {'name': 'Phyllis', 'age': 100}
+    json_person = json.dumps(person)
+    return (json_person, 200, None)
+
+
 if __name__ == '__main__':
     app.run()
-    app.config["DEBUG"] = True
+    app.config['DEBUG'] = True
